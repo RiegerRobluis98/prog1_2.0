68900f8d5ba0eb55765d80d8766881758eb0e94c
Luiz Roberto C. Rieger <45156462+RiegerRobluis98@users.noreply.github.com>
2019-08-20 23:00:12 -0300

    Update exeIuri.java

--- exeIuri.java
+++ exeIuri.java
@@ -1,14 +1,13 @@
 import javax.swing.JOptionPane;
 
 public class JavaApplication3 {
-    public static void main(String[] args) {
-      String nome, sobrenome, endereco, cpf;
-      nome = JOptionPane.showInputDialog("Qual seu nome");
-      sobrenome = JOptionPane.showInputDialog("Qual seu Sobrenome? ");
-       cpf = JOptionPane.showInputDialog("Qual seu CPF?");
-       endereco = JOptionPane.showInputDialog("Qual seu Endereco?");
-    JOptionPane.showMessageDialog(null, nome +" \n"+ sobrenome + " \n" + endereco + " \n" + cpf);
-   
+     public static void main(String[] args) {
+String nome, sobrenome, cpf, endereco;
+nome = JOptionPane.showInputDialog("Qual seu nome?");
+sobrenome = JOptionPane.showInputDialog("Qual seu sobrenome?");
+cpf = JOptionPane.showInputDialog("Qual o Numero do seu cpf?");
+endereco = JOptionPane.showInputDialog("Qual seu endereco?");
+JOptionPane.showMessageDialog(null,"Nome Completo: " + nome +" "+ sobrenome + "\n Endereco: " + endereco + "\n CPF: " + cpf);
     }
     
 }
